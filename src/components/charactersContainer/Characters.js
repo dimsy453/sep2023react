3fc9eefb3b9e286d5ccc06238bb9add229cebb84
dimsy453 <dimsy453@gmail.com>
2024-02-22 20:46:18 +0200

    p_1

---
+++ src/components/charactersContainer/Characters.js
@@ -0,0 +1,20 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {characterService} from "../../services";
+import {Character} from "./character";
+
+const Characters = () => {
+    const [characters, setCharacters] = useState([])
+    const {ids} = useParams();
+
+    useEffect(() => {
+        characterService.getById(ids).then(({data}) => setCharacters(data))
+    }, [ids]);
+    return (
+        <div>
+            {characters.map(character =><Character key={character.id} character={character}/>)}
+        </div>
+    );
+};
+
+export {Characters}
